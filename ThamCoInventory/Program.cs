b6604d9ce9f8a0429347f8684c283f05a06f9473
Michael Small <s6065431@live.tees.ac.uk>
2020-01-07 11:54:23 +0000

    Fix data seeding

--- ThamCoInventory/Program.cs
+++ ThamCoInventory/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Inventorydata.data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace ThamCoInventory
@@ -14,7 +17,29 @@ namespace ThamCoInventory
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var env = services.GetRequiredService<IHostingEnvironment>();
+                if (env.IsDevelopment())
+                {
+                    var context = services.GetRequiredService<InventoryDbContext>();
+                    context.Database.Migrate();
+                    try
+                    {
+                        InventoryDbInitialiser.SeedTestData(context, services).Wait();
+                    }
+                    catch (Exception)
+                    {
+                        var logger = services.GetRequiredService<ILogger<Program>>();
+                        logger.LogDebug("Seeding test data failed.");
+                    }
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
@@ -22,3 +47,4 @@ namespace ThamCoInventory
                 .UseStartup<Startup>();
     }
 }
+    
